c2855fbbfb5acfad98ec0acd8c337d1c3eeddb9e
kim202105 <98707613+kim202105@users.noreply.github.com>
2022-02-18 13:27:41 -0500

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -33,7 +33,7 @@ export default ['index', 'main-room'].map((name, index) => ({
         nodeResolve(),
         replace({
             preventAssignment: true,
-            'https://resources.realitymedia.digital/vue-apps/': componentPath //JSON.stringify( componentPath )
+            'https://kim202105.github.io/vue-apps/': componentPath //JSON.stringify( componentPath )
         }),  
         typescript({
             typescript: require('typescript'),
